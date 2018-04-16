b60b5da850863beeffeedf1071df57f9df0fcca9
young360 <37890716+young360@users.noreply.github.com>
2018-04-16 14:20:56 +0800

    Update Home.js
    Add register function

--- features/pageobjects/Home.js
+++ features/pageobjects/Home.js
@@ -6,6 +6,15 @@ class Home extends Page {
  get myAccount () { return $(`.profile-access__title`) }
  get signIn () { return $x('//*[text()="Sign in"][@type="button"]')}
  get register () { return $(`.btn btn--primary`)}
+ get registerButton() { return $(`.myaccount__login a.btn--primary`)}
+ get registerEmail() { return $(`#email_address`)}
+ get FirstName() { return $(`#first_name`)}
+ get LastName() { return $(`#last_name`)}
+ get NewPassword() { return $(`#reg_password`)}
+ get Country() { return $(`#country`)}
+ get Iagree() { return $(`#agreement_chk_indicator`)}
+ get registersubmit() { return $(`#register-submit`)}
+ get Thankyou() { return $(`.pwc-regis__content-heading`)}
 }
 
 module.exports = Home
